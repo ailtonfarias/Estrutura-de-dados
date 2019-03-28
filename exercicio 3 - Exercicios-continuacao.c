182e75de252a2e01dbd0583f3f679bdef39c245e
ailtonfarias <38967545+ailtonfarias@users.noreply.github.com>
2019-03-28 04:06:36 -0400

    Add files via upload

---
+++ exercicio 3 - Exercicios-continuacao.c
@@ -0,0 +1,22 @@
+/*exercicio 3 parte II*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2;
+    printf("Digite a dimencao da matriz linha/coluna\n");
+    scanf("%d%d",&num1,&num2);
+    tam_matriz(num1,num2);
+
+
+}
+
+
+int tam_matriz(int num1,int num2) {
+
+printf("A dimensao da matriz e de %d %d ",num1,num2);
+}
+
+
+
